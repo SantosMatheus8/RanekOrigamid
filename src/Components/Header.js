5a398811bc31b385be621434c943312a06ed6f7b
Matheus Pereira <matheus.melo2008@hotmail.com>
2022-05-15 15:30:28 -0300

    Criacao da barra de navegacao

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import styles from "./css/Header.module.css";
 
 const Header = () => {
   return (
-    <div>
-      <h1>HEADER</h1>
-    </div>
+    <nav className={styles.header}>
+      <ul>
+        <li>
+          <NavLink className={styles.link} to="/" end>
+            PRODUTOS
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className={styles.link} to="contato">
+            CONTATO
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
