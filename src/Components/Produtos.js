bc2cfff4ef063108098be95620ffb19916499d52
Matheus Pereira <matheus.melo2008@hotmail.com>
2022-05-16 09:07:42 -0300

    Criacao da tela de produtos

--- src/Components/Produtos.js
+++ src/Components/Produtos.js
@@ -1,8 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./css/Produtos.module.css";
 
 const Produtos = () => {
-  return <div>PRODUTOS</div>;
+  const [produtos, setProdutos] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("https://ranekapi.origamid.dev/json/api/produto").then((response) =>
+      response.json().then((json) => setProdutos(json))
+    );
+  }, []);
+  if (produtos === null) return null;
+  console.log(produtos);
+  return (
+    <section className={styles.produtos + " animeLeft"}>
+      {produtos.map((produto) => (
+        <Link to={`produto/${produto.id}`} key={produto.id}>
+          <img src={produto.fotos[0].src} alt={produto.fotos[0].titulo} />
+          <p className={styles.nome}>{produto.nome}</p>
+        </Link>
+      ))}
+    </section>
+  );
 };
 
 export default Produtos;
