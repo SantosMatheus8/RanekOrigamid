de7f8992f7cdd3457cadb2621e6ddcbba784742f
Matheus Pereira <matheus.melo2008@hotmail.com>
2022-05-15 22:36:58 -0300

    Criando a pagina de contato

--- src/Components/Contato.js
+++ src/Components/Contato.js
@@ -1,8 +1,21 @@
 import React from "react";
 import styles from "./css/Contato.module.css";
+import imagem from "../img/contato.jpg";
 
 const Contato = () => {
-  return <div>CONTATO</div>;
+  return (
+    <section className={styles.contato}>
+      <img src={imagem} alt="Máquina de escrever" className={styles.img} />
+      <div>
+        <h1>Entre em contato</h1>
+        <ul className={styles.informacoes}>
+          <li>matheus.melo2008@hotmail.com</li>
+          <li>(62)99999-9999</li>
+          <li>Rua 239</li>
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default Contato;
